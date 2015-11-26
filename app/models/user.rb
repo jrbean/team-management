5cfecd4a66dbc47ce70385f5ee47090f6f8040e8
Justin <justin@zigfield.home>
2015-11-26 15:01:36 -0500

    additional devise username config

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/
-
+  validate :validate_username
   attr_accessor :login
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
